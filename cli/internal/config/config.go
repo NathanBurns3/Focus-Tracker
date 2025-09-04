e3f436f632934c3a0e3b398a555a65c7560d272c
Nathan Burns <nate10nate@gmail.com>
2025-09-04 15:47:38 -0400

    Add configuration management for application settings

---
+++ cli/internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+// Config holds the configuration values for the application
+type Config struct {
+	DbPath        	string // Path to the database file
+	PollingSeconds 	int    // Polling interval in seconds
+}
+
+// Load read configuration from environment variables and returns a Config struct
+// If DB_PATH is not set, it logs a warning and runs in dry mode
+func Load() *Config {
+	cfg := Config{
+		DbPath:         os.Getenv("DB_PATH"), //TODO: set up env file with DB_PATH
+		PollingSeconds: 10,
+	}
+
+	if cfg.DbPath == "" {
+		log.Println("WARNING: DB_PATH not set, running in dry mode.")
+	}
+
+	return &cfg
+}
