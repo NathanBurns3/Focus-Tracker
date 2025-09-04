6b33c06815a66d0f8b7761be06b61064c297f16b
Nathan Burns <nate10nate@gmail.com>
2025-09-04 14:41:17 -0400

    Implement intial polling functionality in the daemon package

--- cli/internal/daemon/daemon.go
+++ cli/internal/daemon/daemon.go
@@ -1,1 +1,17 @@
 package daemon
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func StartPolling() {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		// Placeholder for AppleScript call
+		fmt.Println("Polling for active application...")
+	}
+}
