aa8cc5a369ffdca8f01c2462301ce82b9d787e56
Nathan Burns <nate10nate@gmail.com>
2025-09-04 15:47:54 -0400

    Refactor stop command implementation to provide clear descriptions and improve user guidance

--- cli/cmd/stop.go
+++ cli/cmd/stop.go
@@ -6,31 +6,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// stopCmd represents the stop command
+// stopCmd defines the "stop" command for the CLI
+// This command is intended to stop the background daemon
 var stopCmd = &cobra.Command{
 	Use:   "stop",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Stop background daemon",
+	Long:  "Stop the background daemon that polls for the active application every 10 seconds.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("stop called")
+		fmt.Println("Stopping tracker daemon...")
+		// TODO: Implement stop functionality
 	},
 }
 
+// init registers the stopCmd with the root command
 func init() {
 	rootCmd.AddCommand(stopCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// stopCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// stopCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
